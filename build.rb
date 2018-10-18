63988346d48e02838631802850a7f6a315337bef
Tadashi Saito <tad.a.digger@gmail.com>
2018-10-18 10:24:41 +0900

    initial

---
+++ build.rb
@@ -0,0 +1,28 @@
+require 'rest-client'
+
+BASE = -'https://fonts.googleapis.com/css?family='
+FAMLIY = -ENV.fetch('FAMLIY')
+PUBLISH_DIR = -'public'
+CSS_FILE_NAME = -'google-fonts.css'
+INJECTATE = -'font-display: swap;'
+
+def main
+  css = get(BASE + FAMILY)
+  modified = modify(css)
+  save(CSS_FILE_NAME, modified)
+end
+
+def get(url)
+  RestClinet.get(url)
+end
+
+def modify(css)
+  subed = INJECTATE + '}'
+  css.gsub!('}', subed)
+  css
+end
+
+def save(css, name)
+  path = PUBLISH_DIR + '/' + CSS_FILE_NAME
+  File.write(path, css)
+end
